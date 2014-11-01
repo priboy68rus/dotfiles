c7a94fac4f00cb44b22394ac8f9142a7026d48b1
priboy68rus <john@MacBook-Pro-Kirill.local>
2014-11-01 22:27:36 +0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##############################################################################
+# setup.sh 
+# Setup all dotfiles from github and move old files to dotfiles_old
+##############################################################################
+ 
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files="zshrc oh-my-zsh vimrc vim tmux.conf"
+
+
+# Creating dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "..done"
+
+for file in $files do
+		echo "Moving old dotfile to $olddir"
+		mv ~/.$file $olddir
+		echo "Creating symlink to $file in home dir"
+		ln -s $dir/$file ~/.$file
+done
